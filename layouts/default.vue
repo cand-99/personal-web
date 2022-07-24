a7f26755810ba79dd2b680356ecf0855754fccbb
Candra <candraherdiana78@gmail.com>
2022-07-24 17:09:49 +0700

    add directory structure

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        from defalut
+         <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        from page/ index
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
