02a3de4ca5b1eb6b626c22feefab349ff84e2e59
Candra <candraherdiana78@gmail.com>
2022-07-24 17:42:03 +0700

    add headlessui & heroicons, & Head Management

--- app.vue
+++ app.vue
@@ -5,3 +5,14 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Site Title` : "Site Title";
+  },
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [{ name: "description", content: "My amazing site." }],
+});
+</script>
