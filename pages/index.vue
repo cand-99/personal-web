42953cc90d255fcc785863dbb9376a44625be85e
Candra <candraherdiana78@gmail.com>
2022-08-08 22:17:39 +0700

    add button component

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-
+import { ArrowRightIcon } from "@heroicons/vue/solid";
 definePageMeta({
   pageTransition: {
     name: "fade",
     mode: "out-in",
   },
 });
-
 </script>
 
 <template>
-  <div class="text-center max-w-3xl mx-auto bg-white bg-opacity-20 dark:bg-opacity-0 dark:backdrop-blur-0 backdrop-blur-xl p-32 rounded-3xl">
+  <div
+    class="text-center max-w-3xl mx-auto bg-white bg-opacity-20 dark:bg-opacity-0 dark:backdrop-blur-0 backdrop-blur-xl p-32 rounded-3xl"
+  >
     <p
       class="inline-flex items-center text-center text-lg md:text-2xl text-gray-500 dark:text-gray-300 tracking-wide"
     >
@@ -18,20 +19,13 @@ definePageMeta({
       <span class="wave text-5xl mx-3 max-2">👋🏻</span>
       I am
     </p>
-    <h1 class="text-6xl md:text-8xl font-bold my-4 tracking-tight">
-      Candra
-    </h1>
+    <h1 class="text-6xl md:text-8xl font-bold my-4 tracking-tight">Candra</h1>
     <p
       class="mt-6 text-lg md:text-xl text-center text-gray-500 dark:text-gray-300 tracking-wide"
     >
       Frontend Developer | IndieHacker wannabe
     </p>
-    <div class="mt-12">
-      <NuxtLink to="/about" class="group mt-12 md:mt-24 bg-gray-900 dark:bg-black py-3 px-6 text-white rounded-xl text-xl font-medium">
-        About
-      </NuxtLink>
-    </div>
-
+    <Button title="About" to="/about" />
   </div>
 </template>
 
