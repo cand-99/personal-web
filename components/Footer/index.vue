8dc84977a8710ecf3ae01e651b3726faacdea417
Candra <candraherdiana78@gmail.com>
2022-08-09 21:31:27 +0700

    footer text update

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -2,8 +2,7 @@
 
 <template>
   <div class="py-6 text-xs md:text-sm mx-auto md:mx-0">
-    Copyright © 2022 Candra. All rights reserved. Made with
-    <span class="text-red-500">❤</span>
+    © All rights reserved. Made with ❤️ by Candra Herdiana
   </div>
 </template>
 
