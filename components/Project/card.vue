6e71abc4dedb1a9e8ef5d931ce19d634705fd199
Candra <candraherdiana78@gmail.com>
2022-08-16 11:22:18 +0700

    change card project style

--- components/Project/card.vue
+++ components/Project/card.vue
@@ -21,7 +21,7 @@ const { t } = useLang();
     class="max-w-4xl bg-white bg-opacity-50 dark:bg-opacity-0 dark:backdrop-blur-0 backdrop-blur-xl mx-auto p-4 md:p-7 rounded-3xl flex flex-col md:flex-row"
   >
     <figure
-      class="max-w-xl max-h-80 h-full w-full md:w-8/12 rounded-2xl overflow-hidden"
+      class="max-w-xl max-h-80 h-full w-full md:w-8/12 rounded-2xl overflow-hidden my-auto"
     >
       <img
         class="object-contain w-full h-full"
@@ -33,11 +33,14 @@ const { t } = useLang();
       class="w-full md:w-4/12 mt-4 md:mt-0 ml-0 md:ml-8 flex flex-col justify-center"
     >
       <h1 class="text-2xl md:text-3xl font-bold mb-3">{{ props.title }}</h1>
-      <p class="text-sm md:text-base">
+      <p class="text-sm">
         {{ props.description }}
       </p>
-      <div class="flex space-x-2 p-3 mb-2 text-2xl text-gray-600 dark:text-gray-300">
-        <nuxt-icon v-for="tool in props.tools" :name="tool.icon" title="sdad" />
+      <div class="my-3">
+        <h1 class="font-bold mb-1">Technology</h1>
+        <div class="flex space-x-2 pb-3 text-2xl text-gray-600 dark:text-gray-300">
+          <nuxt-icon v-for="tool in props.tools" :name="tool.icon" title="sdad" />
+        </div>
       </div>
       <Button
         :title="$t('pages.project.visit')"
