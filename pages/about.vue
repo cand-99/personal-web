465ce0cf1a1fcc596533d5203d44f74fc6ccde00
Candra <candraherdiana78@gmail.com>
2022-08-23 13:40:03 +0700

    about page done

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,4 @@
 <script lang="ts" setup>
-interface ITools {
-  icon: string;
-  link: string
-}
-interface IAbout {
- name?: string
-}
-
 definePageMeta({
   pageTransition: {
     name: "fade",
@@ -20,121 +12,86 @@ useHead(() => ({
   title: t("pages.about.title"),
 }));
 
-const tools = computed((): ITools[] => [
-  {
-    icon: "vue",
-    link: "https://vuejs.org/",
-  },
-  {
-    icon: "nuxt",
-    link: "https://nuxtjs.org/",
-  },
-  {
-    icon: "tailwindcss",
-    link: "https://tailwindcss.com/",
-  },
-  {
-    icon: "laravel",
-    link: "https://laravel.com/",
-  },
-]);
-
-const abouts = useAbout()
-const isLoading = ref(false)
-
-async function getAbout() {
-  try {
-    isLoading.value = true
-    const data = await $fetch("/api/about");
-    abouts.value = data[0]
-    
-  } catch (error) {
-    console.log(error);
-  } finally {
-    isLoading.value = false
-  }
-}
-
-if(!abouts.value){
-  getAbout()
-}
-
+const { data: abouts, pending, error, refresh } = await useFetch("/api/about");
 </script>
 
 <template>
   <section
     class="text-center max-w-4xl w-full mx-auto bg-white bg-opacity-40 dark:bg-opacity-0 dark:backdrop-blur-0 backdrop-blur-xl py-8 px-1 md:p-32 rounded-3xl z-10"
   >
-    <h1 class="font-bold text-5xl md:text-7xl text-center tracking-tight">
-      {{ abouts.name }}
-    </h1>
-    <h2
-      class="mt-4 text-xl md:text-2xl text-center text-gray-500 dark:text-gray-300"
-    >
-      Self-taught Frontend Developer <br />
-    </h2>
-    <div>
-      <div class="text-left mt-4">
-        <h1 class="title">{{ $t('pages.about.basic') }}</h1>
-        <p class="text-gray-500 dark:text-gray-400 text-sm md:text-base">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor iusto
-          asperiores ipsam quos, ipsum placeat voluptas praesentium maxime
-          dignissimos eum impedit ea laborum tempore quidem? Non a laborum
-          blanditiis molestiae inventore? Ab quas voluptatum, quidem hic
-          perspiciatis necessitatibus ducimus provident!
-        </p>
-      </div>
-      <div class="flex text-left mt-6 justify-between flex-col md:flex-row">
-        <div class="w-full md:w-2/5">
-          <h1 class="title">{{ $t('pages.about.framework') }}</h1>
-          <p
-            class="flex flex-wrap text-gray-400 dark:text-gray-300 text-4xl gap-3"
-          >
-            <a
-              v-for="tool in tools"
-              :key="tool.icon"
-              :href="tool.link"
-              :aria-label="tool.icon"
-              class="hover:text-gray-600 dark:hover:text-gray-100"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <nuxt-icon :name="tool.icon" />
-            </a>
+    <template v-for="about in abouts">
+      <h1 class="font-bold text-5xl md:text-7xl text-center tracking-tight">
+        {{ about.name }}
+      </h1>
+      <h2
+        class="mt-4 text-xl md:text-2xl text-center text-gray-500 dark:text-gray-300"
+      >
+      {{ about.role }} <br />
+      </h2>
+      <div>
+        <div class="text-left mt-4">
+          <h1 class="title">{{ $t("pages.about.basic") }}</h1>
+          <p class="text-gray-500 dark:text-gray-400 text-sm md:text-base">
+            {{ about.basic }}
           </p>
         </div>
-        <div class="w-full md:w-2/5 mt-6 md:mt-0">
-          <h1 class="title">{{ $t('pages.about.contact') }}</h1>
-          <ul class="text-gray-500 dark:text-gray-400 text-sm md:text-base">
-            <li>Tasikmalaya, Jawabarat</li>
-            <li>99cand@gmail.com</li>
-            <li
-              class="flex gap-4 mt-4 text-2xl md:text-3xl text-gray-400 dark:text-gray-300"
+        <div class="flex text-left mt-6 justify-between flex-col md:flex-row">
+          <div class="w-full md:w-2/5">
+            <h1 class="title">{{ $t("pages.about.framework") }}</h1>
+            <p
+              class="flex flex-wrap text-gray-400 dark:text-gray-300 text-4xl gap-3"
             >
               <a
-                href="https://github.com/cand-99"
+                v-for="tool in about.framework"
+                :key="tool.icon"
+                :href="tool.link"
+                :aria-label="tool.icon"
                 class="hover:text-gray-600 dark:hover:text-gray-100"
                 target="_blank"
-                aria-label="github"
                 rel="noopener noreferrer"
               >
-                <nuxt-icon name="github" />
+                <nuxt-icon :name="tool.icon" />
               </a>
-              <a
-                href="https://www.instagram.com/candfpv"
-                class="hover:text-gray-600 dark:hover:text-gray-100"
-                target="_blank"
-                aria-label="instagram"
-                rel="noopener noreferrer"
+            </p>
+          </div>
+          <div class="w-full md:w-2/5 mt-6 md:mt-0">
+            <h1 class="title">{{ $t("pages.about.contact") }}</h1>
+            <ul class="text-gray-500 dark:text-gray-400 text-sm md:text-base">
+              <li>{{ about.address }}</li>
+              <li>{{ about.email }}</li>
+              <li
+                class="flex gap-4 mt-4 text-2xl md:text-3xl text-gray-400 dark:text-gray-300"
               >
-                <nuxt-icon name="instagram" />
-              </a>
-            </li>
-          </ul>
+                <a
+                  href="https://github.com/cand-99"
+                  class="hover:text-gray-600 dark:hover:text-gray-100"
+                  target="_blank"
+                  aria-label="github"
+                  rel="noopener noreferrer"
+                >
+                  <nuxt-icon name="github" />
+                </a>
+                <a
+                  href="https://www.instagram.com/candfpv"
+                  class="hover:text-gray-600 dark:hover:text-gray-100"
+                  target="_blank"
+                  aria-label="instagram"
+                  rel="noopener noreferrer"
+                >
+                  <nuxt-icon name="instagram" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-    <Button :title="t('pages.project.nav')" icon="arrow-right" to="/project" class="mt-12" />
+      <Button
+        :title="t('pages.project.nav')"
+        icon="arrow-right"
+        to="/project"
+        class="mt-12"
+      />
+    </template>
   </section>
 </template>
 
--- server/api/about.ts
+++ server/api/about.ts
@@ -4,10 +4,8 @@ export default eventHandler(async (event) => {
   const supabase = serverSupabaseClient(event)
 
   const data = await supabase.from('about').select().eq('id', 1)
-  console.log(data);
   
-  
-  if (!data.body.length) {
+  if (!data.body) {
     sendError(event, createError({ statusCode: 401, statusMessage: 'Unauthenticated' }))
   }
   return data.data
